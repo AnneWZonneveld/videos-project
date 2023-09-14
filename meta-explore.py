5a60bc3c6fcf2771e93ded68651615be88482059
AnneWZonneveld <annewzonneveld@gmail.com>
2023-09-14 16:28:31 +0200

    test/train split

--- meta-explore.py
+++ meta-explore.py
@@ -19,62 +19,99 @@ img_folder = '/scratch/azonneveld/meta-explore'
 # Inspect data structure
 md.head()
 col_names = md.columns.values.tolist()
-n_trials = md.shape[0]
+total_n_trials = md.shape[0]
 
-# Create count df for different label types 
+# Create count df for different label types train and test set
 zets = ['train', 'test']
 vars_oi = ['objects', 'scenes', 'actions']
-vars_dict = {}
 
-for var in vars_oi:
-    temp_df = md[var].to_numpy()
-    all_labels = np.empty(5*n_trials, dtype=object)
+zets_dict = {}
+for zet in zets:
+    temp_zet = md[md['set']==zet]
+    n_trials = temp_zet.shape[0]
+    vars_dict = {}
 
-    for i in range(5*n_trials):
-        trial = i // 5
-        j = i % 5 
-        all_labels[i] = temp_df[trial][j]
-    
-    label_unique, label_counts = np.unique(all_labels, return_counts=True)
-    label_unique = np.expand_dims(label_unique, axis = 1)
-    label_counts = np.expand_dims(label_counts, axis = 1)
+    for var in vars_oi:
+        temp_df = temp_zet[var].to_numpy()
+        all_labels = np.empty(5*n_trials, dtype=object)
 
-    count_df = pd.concat([pd.DataFrame(label_unique), pd.DataFrame(label_counts)], axis=1)
-    count_df.columns = ['label', 'count']
-    count_df = count_df.sort_values(by='count', axis=0, ascending=False)
+        for i in range(5*n_trials):
+            trial = i // 5
+            j = i % 5 
+            all_labels[i] = temp_df[trial][j]
+        
+        label_unique, label_counts = np.unique(all_labels, return_counts=True)
+        label_unique = np.expand_dims(label_unique, axis = 1)
+        label_counts = np.expand_dims(label_counts, axis = 1)
 
-    vars_dict[var] = count_df
+        count_df = pd.concat([pd.DataFrame(label_unique), pd.DataFrame(label_counts)], axis=1)
+        count_df.columns = ['label', 'count']
+        count_df = count_df.sort_values(by='count', axis=0, ascending=False)
 
-# unique labels: objects 771, scenes 323, actions 260 --> biased comparison?
+        vars_dict[var] = count_df
+
+    zets_dict[zet] = vars_dict
 
 # Descriptives 
-labels_max = []
-for var in vars_oi:
-    total_n = vars_dict[var].shape[0]
-    max = vars_dict[var]['count'].iloc[0]
-    labels_max.append(max)
-    print(f'{var} total unique labels: {total_n}')
-    print(f'{var} top 10:')
-    top_10 = vars_dict[var].iloc[0:10]
-    print(f'{top_10}')
-gen_max = np.max(np.array(labels_max))
+zets_max = {}
+for zet in zets:
+    vars_dict = zets_dict[zet]
+    labels_max = []
 
-# Plot label counts
-fig, ax = plt.subplots(1,3, dpi = 200, sharey=True)
-for i in range(len(vars_oi)):
-    var = vars_oi[i]
-    total_n = vars_dict[var].shape[0]
+    for var in vars_oi:
+        total_n = vars_dict[var].shape[0]
+        max = vars_dict[var]['count'].iloc[0]
+        labels_max.append(max)
+        print(f'{zet} {var} unique labels: {total_n}')
+        print(f'{zet} {var} top 10:')
+        top_10 = vars_dict[var].iloc[0:10]
+        print(f'{top_10}')
 
-    sns.barplot(x='label', y='count', data=vars_dict[var], ax=ax[i])
-    ax[i].set_title(f'{var}: {total_n}')
-    ax[i].set_xticks([])
-    ax[i].set_xlabel('Labels')
+    zets_max[zet] = np.max(np.array(labels_max))
+
+# Plot all label counts
+fig, ax = plt.subplots(2,3, dpi = 300, sharey='row')
+for j in range(len(zets)):
+    zet = zets[j]
+    vars_dict = zets_dict[zet]
+
+    for i in range(len(vars_oi)):
+        var = vars_oi[i]
+        total_n = vars_dict[var].shape[0]
+
+        sns.barplot(x='label', y='count', data=vars_dict[var], ax=ax[j, i])
+        ax[j, i].set_title(f'{zet} {var}: {total_n}')
+        ax[j, i].set_xticks([])
+        ax[j, i].set_xlabel('Labels')
 
 fig.tight_layout()
 img_path = img_folder + '/label-hist.png'
 plt.savefig(img_path)
 plt.clf()
-# Highly skewed distirubions
+
+# Plot top 15 label counts
+fig, ax = plt.subplots(2,3, dpi = 200, sharex='row')
+fig.set_figwidth(12)
+for j in range(len(zets)):
+    zet = zets[j]
+    vars_dict = zets_dict[zet]
+
+    for i in range(len(vars_oi)):
+        var = vars_oi[i]
+        total_n = vars_dict[var].shape[0]
+
+        sns.barplot(x='count', y='label', data=vars_dict[var].iloc[0:15], ax=ax[j, i])
+        ax[j, i].set_xlabel('count')
+        ax[j, i].set_ylabel('')
+        ax[j,i].set_title(f'{zet} {var}: {total_n}')
+        ax[j,i].tick_params(axis='y', labelsize=6)
+
+fig.tight_layout()
+img_path = img_folder + '/label-hist-top.png'
+plt.savefig(img_path)
+plt.clf()
+
+# ----------------------------------------------------------
 
 shell()
 
