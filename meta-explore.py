81a4dd613060dda3eb494ad17348699a46059802
AnneWZonneveld <annewzonneveld@gmail.com>
2023-09-19 12:14:58 +0200

    bokeh overview plots

--- meta-explore.py
+++ meta-explore.py
@@ -146,8 +146,8 @@ shell()
 import fasttext
 import pickle
 import bokeh.plotting as bp
-from bokeh.models import HoverTool, BoxSelectTool
-from bokeh.plotting import figure, show, output_notebook
+from bokeh.models import HoverTool, BoxSelectTool, ColumnarDataSource, LinearColorMapper
+from bokeh.layouts import row, column, layout
 from sklearn.manifold import MDS
 import numpy as np
 import pandas as pd
@@ -160,38 +160,106 @@ ft = fasttext.load_model('./downloads/cc.en.300.bin')
 with open('/scratch/azonneveld/meta-explore/freq_data.pkl', 'rb') as f:
     freq_data = pickle.load(f)
 
+# # %% Get word vectors for labels
+# vars_dict = freq_data['train']
+# labels = np.unique(vars_dict['actions']['label'])
+# n_labels = len(labels)
+
+# wv = np.empty((n_labels, 300), dtype='object')
+# for i in range(n_labels):
+#     label = labels[i]
+#     c_vect = ft.get_word_vector(label)
+#     wv[i, :] = c_vect
+
+# mds_model = MDS(n_components=2, random_state=0)
+# mds_ft = mds_model.fit_transform(wv)
+# mds_df = pd.DataFrame(mds_ft, columns=['x', 'y'])
+# mds_df['words'] = list(labels)
+# mds_df['count'] = list(vars_dict['actions']['count'])
+
+# mds_plot = bp.figure(plot_width=500, plot_height=400, title="FastText Action labels",
+# tools="pan,wheel_zoom,box_zoom,reset,hover",
+# x_axis_type=None, y_axis_type=None, min_border=1)
+# color_mapper = LinearColorMapper(palette='Plasma256', low=min(mds_df['count']), high=max(mds_df['count']))
+# mds_plot.scatter(x='x', y='y', source=mds_df, color={'field': 'count', 'transform': color_mapper})
+# hover = mds_plot.select(dict(type=HoverTool))
+# hover.tooltips={"word": "@words",
+#                 "count": "@count"}
+# bp.output_file(filename="./meta-explore/mds-test.html", title="mds-test")
+# bp.save(mds_plot)
+
+# try:
+#     bp.reset_output()
+#     bp.output_notebook()
+#     bp.show(mds_plot)
+# except:
+#     bp.output_notebook()
+#     bp.show(mds_plot)
+
+
 # %% Get word vectors for labels
-vars_dict = freq_data['train']
-labels = np.unique(vars_dict['actions']['label'])
-n_labels = len(labels)
 
-wv = np.empty((n_labels, 300), dtype='object')
-for i in range(n_labels):
-    label = labels[i]
-    c_vect = ft.get_word_vector(label)
-    wv[i, :] = c_vect
+zets = ['train', 'test']
+vars = ['objects', 'scenes', 'actions']
 
-# %% MDS plot
-mds_model = MDS(n_components=2, random_state=0)
-mds_ft = mds_model.fit_transform(wv)
-mds_df = pd.DataFrame(mds_ft, columns=['x', 'y'])
-mds_df['words'] = list(labels)
+mds_plots = []
+wv_dict = {}
 
-mds_plot = bp.figure(plot_width=500, plot_height=400, title="FT Action labels",
-tools="pan,wheel_zoom,box_zoom,reset,hover",
-x_axis_type=None, y_axis_type=None, min_border=1)
-mds_plot.scatter(x='x', y='y', source=mds_df,color = 'darkcyan')
-hover = mds_plot.select(dict(type=HoverTool))
-hover.tooltips={"word": "@words"}
-bp.output_file(filename="./meta-explore/mds-test.html", title="mds-test")
-bp.save(mds_plot)
+# Loop through sets and variables
+for zet in zets:
+    temp_wv_dict = {}
+
+    for var in vars:
+
+        # Extract embeddings
+        c_dict = freq_data[zet][var]
+        labels = np.unique(c_dict['label'])
+        n_labels = len(labels)
+
+        wv = np.empty((n_labels, 300), dtype='object')
+        for i in range(n_labels):
+            label = labels[i]
+            c_vect = ft.get_word_vector(label)
+            wv[i, :] = c_vect
+
+        # Store embeddings
+        temp_wv_dict[var] = wv
+
+        # Perform MDS 
+        print(f"Performing MDS for {zet} {var}")
+        mds_model = MDS(n_components=2, random_state=0)
+        mds_ft = mds_model.fit_transform(wv)
+        mds_df = pd.DataFrame(mds_ft, columns=['x', 'y'])
+        mds_df['words'] = list(labels)
+        mds_df['count'] = list(c_dict['count'])
+
+        mds_plot = bp.figure(plot_width=500, plot_height=400, title=f"FastText {zet} {var} labels",
+        tools="pan,wheel_zoom,box_zoom,reset,hover",
+        x_axis_type=None, y_axis_type=None, min_border=1)
+        color_mapper = LinearColorMapper(palette='Plasma256', low=min(mds_df['count']), high=max(mds_df['count']))
+        mds_plot.scatter(x='x', y='y', source=mds_df, color={'field': 'count', 'transform': color_mapper})
+        hover = mds_plot.select(dict(type=HoverTool))
+        hover.tooltips={"word": "@words",
+                        "count": "@count"}
+        
+        mds_plots.append(mds_plot)
+
+    wv_dict[zet] = temp_wv_dict
+
+# %% Show figure
+complete_fig = layout([[mds_plots[0], mds_plots[1], mds_plots[2]],
+        [mds_plots[3], mds_plots[4], mds_plots[5]]
+])
+
+bp.output_file(filename="./meta-explore/mds-overview.html", title="mds-overview")
+bp.save(complete_fig)
 
 try:
     bp.reset_output()
     bp.output_notebook()
-    bp.show(mds_plot)
+    bp.show(complete_fig)
 except:
     bp.output_notebook()
-    bp.show(mds_plot)
+    bp.show(complete_fig)
 
 # %%
