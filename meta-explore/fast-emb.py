9a976c46c473071cec89546d9c3463b7d81f32f0
AnneWZonneveld <annewzonneveld@gmail.com>
2023-09-27 15:15:03 +0200

    updated

--- meta-explore/fast-emb.py
+++ meta-explore/fast-emb.py
@@ -21,10 +21,12 @@ from bokeh.models import HoverTool, BoxSelectTool, ColumnarDataSource, LinearCol
 from bokeh.layouts import row, column, layout
 from sklearn.manifold import MDS
 
-#%%
 
 # Pick 'ft' for default, 'cb_ft' for cbow, 'skip_ft' for skipgram
 model = 'ft'
+
+descript = False
+perform_MDS = True
 
 # Load word model: pretrained vs trained from scratch
 print('Loading word model')
@@ -32,16 +34,16 @@ if model == 'ft':
     ft = fasttext.load_model('/scratch/azonneveld/downloads/cc.en.300.bin')
 elif model == 'cb_ft':
     try:
-        ft = fasttext.load_model('/scratch/azonneveld/downloads/cb.ft.300.bin')
+        ft = fasttext.load_model('/scratch/azonneveld/downloads/wiki.cb.ft.300.bin')
     except:
         ft = fasttext.train_unsupervised('/scratch/azonneveld/downloads/fil9', model='cbow', dim=300)
-        ft.save_model('/scratch/azonneveld/downloads/cb.ft.300.bin')
+        ft.save_model('/scratch/azonneveld/downloads/wiki.cb.ft.300.bin')
 else:
     try:
-        ft = fasttext.load_model('/scratch/azonneveld/downloads/skip.ft.300.bin')
+        ft = fasttext.load_model('/scratch/azonneveld/downloads/wiki.skip.ft.300.bin')
     except:
         ft = fasttext.train_unsupervised('/scratch/azonneveld/downloads/fil9', model='skipgram', dim=300)
-        ft.save_model('/scratch/azonneveld/downloads/skip.ft.300.bin')
+        ft.save_model('/scratch/azonneveld/downloads/wiki.skip.ft.300.bin')
 
 # %% Load freq dictionary 
 with open('/scratch/azonneveld/meta-explore/freq_data.pkl', 'rb') as f:
@@ -52,116 +54,117 @@ with open('/scratch/azonneveld/meta-explore/freq_data.pkl', 'rb') as f:
 zets = ['train', 'test']
 vars = ['objects', 'scenes', 'actions']
 
-mds_plots = []
-wv_dict = {}
+if perform_MDS == True:
 
-# Loop through sets and variables
-for zet in zets:
-    temp_wv_dict = {}
+    # Loop through sets and variables
+    mds_plots = []
+    wv_dict = {}
+    wv_dict_all = {}
+    
+    for zet in zets:
 
-    for var in vars:
+        zet_dict = {}
 
-        # Extract embeddings
-        c_dict = freq_data[zet][var]
-        labels = np.unique(c_dict['label'])
-        n_labels = len(labels)
+        for var in vars:
 
-        wv = np.empty((n_labels, 300), dtype='object')
-        for i in range(n_labels):
-            label = labels[i]
-            c_vect = ft.get_word_vector(label)
-            wv[i, :] = c_vect
+            var_dict = {}
 
-        # Store embeddings
-        temp_wv_dict[var] = wv
+            # Extract embeddings
+            c_dict = freq_data[zet][var]
+            labels = np.unique(c_dict['label'])
+            n_labels = len(labels)
 
-        # Perform MDS 
-        print(f"Performing MDS for {zet} {var}")
-        mds_model = MDS(n_components=2, random_state=0)
-        mds_ft = mds_model.fit_transform(wv)
-        mds_df = pd.DataFrame(mds_ft, columns=['x', 'y'])
-        mds_df['words'] = list(labels)
-        mds_df['count'] = list(c_dict['count'])
+            wv = np.empty((n_labels, 300), dtype='object')
+            for i in range(n_labels):
+                label = labels[i]
+                c_vect = ft.get_word_vector(label)
+                wv[i, :] = c_vect
 
-        mds_plot = bp.figure(plot_width=500, plot_height=400, title=f"{model} {zet} {var} labels",
-        tools="pan,wheel_zoom,box_zoom,reset,hover",
-        x_axis_type=None, y_axis_type=None, min_border=1)
-        color_mapper = LinearColorMapper(palette='Plasma256', low=min(mds_df['count']), high=max(mds_df['count']))
-        mds_plot.scatter(x='x', y='y', source=mds_df, color={'field': 'count', 'transform': color_mapper})
-        hover = mds_plot.select(dict(type=HoverTool))
-        hover.tooltips={"word": "@words",
-                        "count": "@count"}
+                wv_dict_all[label] = c_vect
+                var_dict[label] = c_vect
+
+            zet_dict[var] = var_dict
+
+            # Perform MDS 
+            print(f"Performing MDS for {zet} {var}")
+            mds_model = MDS(n_components=2, random_state=0)
+            mds_ft = mds_model.fit_transform(wv)
+            mds_df = pd.DataFrame(mds_ft, columns=['x', 'y'])
+            mds_df['words'] = list(labels)
+            mds_df['count'] = list(c_dict['count'])
+
+            mds_plot = bp.figure(plot_width=500, plot_height=400, title=f"{model} {zet} {var} labels",
+            tools="pan,wheel_zoom,box_zoom,reset,hover",
+            x_axis_type=None, y_axis_type=None, min_border=1)
+            color_mapper = LinearColorMapper(palette='Plasma256', low=min(mds_df['count']), high=max(mds_df['count']))
+            mds_plot.scatter(x='x', y='y', source=mds_df, color={'field': 'count', 'transform': color_mapper})
+            hover = mds_plot.select(dict(type=HoverTool))
+            hover.tooltips={"word": "@words",
+                            "count": "@count"}
+            
+            mds_plots.append(mds_plot)
         
-        mds_plots.append(mds_plot)
+        wv_dict[zet] = zet_dict
 
-    wv_dict[zet] = temp_wv_dict
+    # Save all wvs
+    with open(f'/scratch/azonneveld/meta-explore/{model}_wv_all.pkl', 'wb') as f:
+            pickle.dump(wv_dict_all, f)
+    with open(f'/scratch/azonneveld/meta-explore/{model}_wv.pkl', 'wb') as f:
+            pickle.dump(wv_dict, f)
 
-# Save all wvs
-with open(f'scratch/azonneveld/meta-explore/{model}_wv', 'wb') as f:
-        pickle.dump(wv_dict, f)
+    # %% Save figure
+    complete_fig = layout([[mds_plots[0], mds_plots[1], mds_plots[2]],
+            [mds_plots[3], mds_plots[4], mds_plots[5]]
+    ])
 
-# %% Save figure
-complete_fig = layout([[mds_plots[0], mds_plots[1], mds_plots[2]],
-        [mds_plots[3], mds_plots[4], mds_plots[5]]
-])
+    bp.output_file(filename=f"/scratch/azonneveld/meta-explore/mds-overview-{model}.html", title="mds-overview")
+    bp.save(complete_fig)
 
-bp.output_file(filename=f"/scratch/azonneveld/meta-explore/mds-overview-{model}.html", title="mds-overview")
-bp.save(complete_fig)
+# Descriptives
+if descript == True:
+    print(f'descriptives for {model}')
+    vocab = ft.words
+    # in_subset = ['home', 'love', 'understand', 'books', 'watch', 'town', 'hockey', 'friend', 'pay', 'need']
+    # out_subset = ['balance beam', 'zen garden', 'storage room', 'art school', 'ski slope', 'baseball player', 'hot pot', 'race car', 'playing music', 'coral reef'] # actually check 
 
-# More evaluations
-vocab = ft.words
-in_subset = ['home', 'love', 'understand', 'books', 'watch', 'town', 'hockey', 'friend', 'pay', 'need']
-out_subset = ['balance beam', 'zen garden', 'storage room', 'art school', 'ski slope', 'baseball player', 'hot pot', 'race car', 'playing music', 'coral reef'] # actually check 
+    # New in set is in wikipedia vocab + in labels of BOLD dataset
+    # Out set is out of wikipedia vocab (but still in labels of BOLD dataset?)
+    in_subset = ['jellyfish', 'limousine', 'orange', 'sunglasses', 'shipwreck', 'bar', 'aquarium', 'walking', 'studying', 'smoking']
+    out_subset = ['balance beam', 'zen garden', 'storage room', 'art school', 'ski slope', 'baseball player', 'hot pot', 'race car', 'playing music', 'coral reef'] 
 
-# Check --> true
-in_checks = []
-out_checks = []
-for i in range(len(out_subset)):
-    in_word = in_subset[i]
-    out_word = out_subset[i]
-    in_check = vocab.count(in_word)
-    out_check = vocab.count(out_word)
-    in_checks.append(in_check)
-    out_checks.append(out_check)
+    # Check --> true
+    in_checks = []
+    out_checks = []
+    for i in range(len(out_subset)):
+        in_word = in_subset[i]
+        out_word = out_subset[i]
+        in_check = vocab.count(in_word)
+        out_check = vocab.count(out_word)
+        in_checks.append(in_check)
+        out_checks.append(out_check)
+    print(f"in_checks: {in_checks}")
+    print(f"out_checks: {out_checks}")
 
-in_neighbours = {}
-out_neighbours = {}
-for i in range(len(in_subset)):
-    in_word = in_subset[i]
-    in_neighbours[in_word] = ft.get_nearest_neighbors(in_word, k=5)
-    out_word = out_subset[i]
-    out_neighbours[out_word] = ft.get_nearest_neighbors(out_word, k=5)
+    in_neighbours = {}
+    out_neighbours = {}
+    for i in range(len(in_subset)):
+        in_word = in_subset[i]
+        in_neighbours[in_word] = ft.get_nearest_neighbors(in_word, k=5)
+        out_word = out_subset[i]
+        out_neighbours[out_word] = ft.get_nearest_neighbors(out_word, k=5)
 
-print(f"{model} in vocab neigh: ")
-print(f"{in_neighbours}")
-print(f"{model} out vocab neigh: ")
-print(f"{out_neighbours}")
+    print(f"{model} in vocab neigh: ")
+    print(f"{in_neighbours}")
+    print(f"{model} out vocab neigh: ")
+    print(f"{out_neighbours}")
 
-ft.get_analogies("berlin", "germany", "france") #paris
-ft.get_analogies("king", "prince", "queen") 
-ft.get_analogies("psx", "sony", "nintendo") # gamecube
-ft.get_analogies("ronaldo", "soccer", "tennis")
-
-
-
-
-
-
-
-
-
-
-#r_subset = np.random.choice(vocab, size=(100,), replace=False)
-
-
-
-
-# shell()
-
-# try:
-#     bp.reset_output()
-#     bp.output_notebook()
-#     bp.show(complete_fig)
-# except:
-#     bp.output_notebook()
-#     bp.show(complete_fig)
+    print('Analogies')
+    a1 = ft.get_analogies("berlin", "germany", "france") #paris
+    a2 = ft.get_analogies("king", "prince", "queen")  # princess
+    a3 = ft.get_analogies("psx", "sony", "nintendo") # gamecube
+    a4 = ft.get_analogies("ronaldo", "soccer", "tennis") #djocovic
+    a5 = ft.get_analogies("italy", "spaghetti", "germany") #curry worst
+    print(a1)
+    print(a2)
+    print(a3)
+    print(a4)
--- meta-explore/guse-emb.py
+++ meta-explore/guse-emb.py
@@ -13,70 +13,40 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
-from IPython import embed as shell
-# import scipy
+import scipy
 import bokeh.plotting as bp
 from bokeh.models import HoverTool, BoxSelectTool, ColumnarDataSource, LinearColorMapper
 from bokeh.layouts import row, column, layout
 from sklearn.manifold import MDS
-from transformers import BertTokenizer, BertModel
+import tensorflow as tf
+import tensorflow_hub as hub
+import re
 from datasets import load_dataset, Dataset, DatasetDict
-import faiss
-from nltk.corpus import stopwords
 
 descript = False
 perform_MDS = True
 
-# Define functions
-def clean_text(x):
-
-    x = str(x)
-    for punct in "/-'":
-        x = x.replace(punct, ' ')
-    for punct in '&':
-        x = x.replace(punct, f' {punct} ')
-    for punct in '?!.,"#$%\'()*+-/:;<=>@[\\]^_`{|}~' + '“”’':
-        x = x.replace(punct, '')
-    return x
-
-STOPWORDS = set(stopwords.words('english'))
-def remove_stopwords(text):
-    """custom function to remove the stopwords"""
-    return " ".join([word for word in str(text).split() if word not in STOPWORDS])
-
-def cls_pooling(model_output):
-    return model_output.last_hidden_state[:, 0]
-
-def get_embeddings(text_list):
-    encoded_input = tokenizer(
-        text_list, padding=True, truncation=True, return_tensors="pt"
-    )
-    encoded_input = {k: v for k, v in encoded_input.items()}
-    model_output = model(**encoded_input)
-    return cls_pooling(model_output)
-
-# Load data and model
+# Load freq data and model
 with open('/scratch/azonneveld/meta-explore/freq_data.pkl', 'rb') as f:
     freq_data = pickle.load(f)
 
-print("Downloading pretrained BERT")
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-model = BertModel.from_pretrained("bert-base-uncased")
-model.eval()
+module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+model = hub.load(module_url)
+print ("module %s loaded" % module_url)
 
 zets = ['train', 'test']
 vars = ['objects', 'scenes', 'actions']
 
-# MDS 
-if perform_MDS == True:
+# Functions
+def embed(input):
+  return model(input)
 
-    # Get word vectors for labels
+
+if perform_MDS == True: 
+    # %% Get word vectors for labels
     mds_plots = []
-    wv_dict_all = {}
     wv_dict = {}
-
-    # # Irrelevant tokens are [CLS], [SEP], + and /
-    # ir_tokens = [101, 102, 1009, 1013]
+    wv_dict_all = {}
 
     for zet in zets:
 
@@ -92,34 +62,11 @@ if perform_MDS == True:
             n_labels = len(labels)
 
             print(f"Preprocessing embeddings {zet} {var}")
-            wv = np.empty((n_labels, 768), dtype='object')
+            wv = np.empty((n_labels, 512), dtype='object')
             
             for i in range(n_labels):
                 label = labels[i]
-
-                # # Mean pooling technique
-                # encoded_input = tokenizer(label, return_tensors='pt')
-                # tokens = encoded_input['input_ids']
-                # # check tokens: 
-                # # tokenizer.tokenize(label)
-
-                # output = model(**encoded_input)
-                # embedding = output.last_hidden_state
-
-                # # Check for (ir)relevant tokens
-                # idx_bool = np.isin(tokens, ir_tokens)[0]
-                # idx =  np.argwhere(idx_bool==False)
-
-                # # Concat relevant and average embeddings
-                # n_idx = idx.shape[0]
-                # concat_emb = np.empty((n_idx, 768))
-                # for j in range(n_idx):
-                #     id = idx[j][0]
-                #     concat_emb[j] = embedding[:, id, :].detach().numpy()
-
-                # av_emb = np.mean(concat_emb, axis=0)
-
-                embedding = get_embeddings(label).detach().numpy()[0]
+                embedding = embed([label]).numpy()[0]
                 wv[i] = embedding
 
                 # Reduce to 300 for fair comparison fasttext?
@@ -149,21 +96,20 @@ if perform_MDS == True:
             mds_plots.append(mds_plot)
         
         wv_dict[zet] = zet_dict
-
+            
     # %% Save figure
     complete_fig = layout([[mds_plots[0], mds_plots[1], mds_plots[2]],
             [mds_plots[3], mds_plots[4], mds_plots[5]]
     ])
 
-    bp.output_file(filename=f"/scratch/azonneveld/meta-explore/mds-overview-bert.html", title="mds-overview")
+    bp.output_file(filename=f"/scratch/azonneveld/meta-explore/mds-overview-guse.html", title="mds-overview")
     bp.save(complete_fig)
 
     # Save all wvs
-    with open(f'/scratch/azonneveld/meta-explore/bert_wv_all.pkl', 'wb') as f:
+    with open(f'/scratch/azonneveld/meta-explore/guse_wv_all.pkl', 'wb') as f:
             pickle.dump(wv_dict_all, f)
-    with open(f'/scratch/azonneveld/meta-explore/bert_wv.pkl', 'wb') as f:
+    with open(f'/scratch/azonneveld/meta-explore/guse_wv.pkl', 'wb') as f:
             pickle.dump(wv_dict, f)
-
 
 # Descriptives
 if descript == True:
@@ -177,41 +123,13 @@ if descript == True:
                 if not label in unique_vocab:
                     unique_vocab.append(label) 
 
-    # # Load wiki corpus
-    # with open('/scratch/azonneveld/downloads/fil9') as f:
-    #     lines = f.readlines()
-
-    # # Preprocess Wiki corpus
-    # strip_text = ''.join(str(word) for word in lines).strip('\n')
-    # cleaned_text = clean_text(strip_text)
-    # no_stop = remove_stopwords(cleaned_text)
-    # vocab = no_stop.split(' ')
-    # unique_vocab = np.unique(np.asarray(vocab))
-
     # Load hugging face dataset for FAISS
-    # ds_dict = {'labels': unique_vocab}
-    # wiki_embs = []
-    # n_vocab = len(unique_vocab)
-    # for i in range(n_vocab): 
-    #     if (i % 100) == 0:
-    #         print(f'{i}')
-    #     label = unique_vocab[i]
-    #     emb = get_embeddings(label).detach().numpy()[0]
-    #     wiki_embs.append(emb)
-    # ds_dict['embeddings'] = wiki_embs
-
-    # # Save all wvs
-    # with open(f'/scratch/azonneveld/meta-explore/bert_wiki wv.pkl', 'wb') as f:
-    #         pickle.dump(ds_dict, f)
-
-    ## Load hugging face dataset for FAISS
     ds_dict = {'labels': unique_vocab}
     ds = Dataset.from_dict(ds_dict)
     embeddings_ds = ds.map(
-        lambda x: {"embeddings": get_embeddings(x['labels']).detach().numpy()[0]}
+        lambda x: {"embeddings": embed([x['labels']]).numpy()[0]}
     )
     embeddings_ds.add_faiss_index(column='embeddings')
-    print('Done preprocessing corpus embeddings')
 
     # Get nearest neighbours for and out vocab
     # in_subset = ['home', 'love', 'understand', 'books', 'watch', 'town', 'hockey', 'friend', 'pay', 'need']
@@ -223,22 +141,20 @@ if descript == True:
     in_neighbours = {}
     out_neighbours = {}
     for i in range(len(in_subset)):
-
         in_word = in_subset[i]
-        in_word_emb = np.expand_dims(np.asarray(get_embeddings(in_word).detach().numpy()[0], dtype=np.float32), axis=0)
+        in_word_emb = np.expand_dims(np.asarray(embed([in_word]).numpy()[0], dtype=np.float32), axis=0)       
         scores, samples = embeddings_ds.get_nearest_examples("embeddings", in_word_emb, k=5)
         samples = samples['labels']
         in_res = (scores, samples)
         in_neighbours[in_word] = in_res
-
         out_word = out_subset[i]
-        out_word_emb = np.expand_dims(np.asarray(get_embeddings(out_word).detach().numpy()[0], dtype=np.float32), axis=0)
+        out_word_emb = np.expand_dims(np.asarray(embed([out_word]).numpy()[0], dtype=np.float32), axis=0)                   
         scores, samples = embeddings_ds.get_nearest_examples("embeddings", out_word_emb, k=5)
         samples = samples['labels']
-        out_res = (scores, samples)               
+        out_res = (scores, samples)
         out_neighbours[out_word] = out_res
 
-    print(f"bert in vocab neigh: ")
+    print(f"{model} in vocab neigh: ")
     print(f"{in_neighbours}")
-    print(f"bert out vocab neigh: ")
+    print(f"{model} out vocab neigh: ")
     print(f"{out_neighbours}")
