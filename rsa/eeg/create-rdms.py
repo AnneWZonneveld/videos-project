312c3ac995f08448f518849bfd12f947042c783f
AnneWZonneveld <annewzonneveld@gmail.com>
2023-11-22 11:59:46 +0100

    before permutation implementation

--- rsa/eeg/create-rdms.py
+++ rsa/eeg/create-rdms.py
@@ -14,21 +14,19 @@ from scipy.stats import pearsonr
 from statsmodels.stats.multitest import multipletests
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
+from sklearn.svm import SVC
 
 # Parameters
 sub = '01'
 zscore = True
 data_split = 'train'
-distance_type = 'euclidean'
+distance_type = 'classification'
+weighted_class = True
 
 # Set random seed for reproducible results
 seed = 20200220
 np.random.seed(seed)
 
-# Helper functions
-def euclidean_distance(a, b):
-    dist = np.linalg.norm(a-b)
-    return dist
 
 ################################### Load EEG data ######################################
 data_dir = f'/scratch/giffordale95/projects/eeg_videos/dataset/preprocessed_data/dataset_02/eeg/sub-{sub}/mvnn-time/baseline_correction-01/highpass-0.01_lowpass-100/sfreq-0050/preprocessed_data.npy'
@@ -69,22 +67,25 @@ stimuli_presentation_order = stimuli_presentation_order[idx]
 # Results array of shape:
 # (Video conditions × Video conditions × EEG time points)
 
-n_perm = 20
+if distance_type ! = 'euclidean':
+    n_perm = 2
+else:
+    n_perm = 1
+
 video_conditions = np.unique(stimuli_presentation_order)
 n_conditions = video_conditions.shape[0]
 n_channels = len(ch_names)
 n_time = times.shape[0]
 rdms_array = np.zeros((n_conditions, n_conditions, eeg_data.shape[2]))
 
-if distance_type == 'euclidean':
-    distance_measure = euclidean_distance
-# to do: other distance measurements
-
 
 # Loop over EEG time points, image-conditions and EEG repetitions
-for t in tqdm(range(eeg_data.shape[2])):
+# for t in tqdm(range(eeg_data.shape[2])):
+for t in tqdm(range(2)):
 
-    # TO DO: implement for loop for n of permutations? 
+    # TO DO: implement for loop for n of permutations? --> only important when using cross validation
+    if distance_type != 'euclidean':
+        permutation_array = np.zeros((n_perm, n_conditions, n_conditions, eeg_data.shape[2]))
 
     for v1 in range(n_conditions):
         for v2 in range(v1):
@@ -126,6 +127,65 @@ for t in tqdm(range(eeg_data.shape[2])):
                 rdms_array[v1, v2, t] = distance
                 rdms_array[v2, v1, t] = distance
 
+                del eeg_cond_1, eeg_cond_2
+            
+            if distance_type == 'euclidean-cv':
+                cv_distances = np.zeros(len(eeg_cond_1))
+
+                for r in range(len(eeg_cond_1)):
+
+                    # Define the training/test partitions (LOOCV)
+                    train_cond_1 = np.delete(eeg_cond_1, r, 0)
+                    train_cond_2 = np.delete(eeg_cond_2, r, 0)
+                    dist_train = np.expand_dims(np.mean(train_cond_1, 0) - np.mean(train_cond_2, 0), 0)
+                    
+                    test_cond_1 = np.expand_dims(eeg_cond_1[r], 0)
+                    test_cond_2 = np.expand_dims(eeg_cond_2[r], 0)
+                    dist_test = test_cond_1  - test_cond_2
+
+                    cv_dist = np.dot(dist_train, np.transpose(dist_test))[0][0] #formula for squared cv euclidean distance?
+                    cv_distances[r] = cv_dist
+                
+                distance = np.mean(cv_distances) 
+
+                # rdms_array[v1, v2, t] = distance
+                # rdms_array[v2, v1, t] = distance
+                permutation_array[]
+            
+            if distance_type == 'classification':
+                y_train = np.zeros(((len(eeg_cond_1)-1)*2))
+                y_train[int(len(y_train)/2):] = 1
+                y_test = np.asarray((0, 1))
+                scores = np.zeros(len(eeg_cond_1))
+
+                for r in range(len(eeg_cond_1)):
+
+                    # Define the training/test partitions
+                    X_train = np.append(np.delete(eeg_cond_1, r, 0),
+                        np.delete(eeg_cond_2, r, 0), 0)
+                    X_test = np.append(np.expand_dims(eeg_cond_1[r], 0),
+                        np.expand_dims(eeg_cond_2[r], 0), 0)
+                    
+                    # Train the classifier
+                    dec_svm = SVC(kernel='linear')
+                    dec_svm.fit(X_train, y_train)
+                    y_pred = dec_svm.predict(X_test)
+
+                    # Test the classifier
+                    if weighted_class == True:
+                        # Weighting of decision values (distance to hyper plane / norm weights), as in gugguenmos et al., 2018
+                        decision_values = dec_svm.decision_function(X_test) / np.linalg.norm(dec_svm.coef_)
+                        weighted_score = sum(decision_values *((y_pred == y_test) - 0.5))/ len(y_test)
+                        scores[r] = weighted_score
+                    else: 
+                        scores[r] = sum(y_pred == y_test) / len(y_test)
+
+                score = np.mean(scores)
+                
+                rdms_array[v1, v2, t] = score
+                rdms_array[v2, v1, t] = score
+
+
 
 # Save results
 results_dict = {
@@ -140,8 +200,8 @@ results_dict = {
 	'info': info
 }
 
-res_folder = f'/scratch/azonneveld/rsa/eeg/rdms/{sub}/' 
-file_path = res_folder + f'{distance_type}.pkl'
+res_folder = f'/scratch/azonneveld/rsa/eeg/rdms/sub-{sub}/' 
+file_path = res_folder + f'{distance_type}_{data_split}.pkl'
 
 # Save all model rdms
 with open(file_path, 'wb') as f:
