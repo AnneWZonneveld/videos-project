f7a902fa108f6a86e6a43de08932b13a5da30a6a
AnneWZonneveld <annewzonneveld@gmail.com>
2024-02-20 22:08:04 +0100

    GA bootstrap participants

--- rsa/fusion/eeg-model/vp_utils.py
+++ rsa/fusion/eeg-model/vp_utils.py
@@ -1,21 +1,22 @@
 import os
+import random
 import pandas as pd
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt 
 import seaborn as sns
 from IPython import embed as shell
-from sklearn.metrics import pairwise_distances
+from sklearn.metrics import pairwise_distances, r2_score
 from scipy.spatial.distance import squareform
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.model_selection import KFold
 from scipy.stats import pearsonr, spearmanr
 from tqdm import tqdm
 import time
 from multiprocessing import shared_memory
 
 
-def corr_nullDist(rdm_cor, rdm_1, rdm_2, its=100):
+def corr_nullDist(rdm_cor, rdm_1, rdm_2, its=100, eval_method='spearman'):
     """
         Calculates null distribution based on permutations. 
     """
@@ -32,10 +33,15 @@ def corr_nullDist(rdm_cor, rdm_1, rdm_2, its=100):
 
         # shuffle RDM consistently for both dims
         shuffled_rdm_1 = rdm_1[shuffle,:] # rows
-        shuffled_rdm_1 = rdm_1[:,shuffle] # columns
+        shuffled_rdm_1 = shuffled_rdm_1[:,shuffle] # columns
 
         # correlating with neural similarty matrix
-        rdm_corr_null.append(spearmanr(squareform(shuffled_rdm_1, checks=False), squareform(rdm_2, checks=False))[0])
+        if eval_method == 'spearman':
+            rdm_corr_null.append(spearmanr(squareform(shuffled_rdm_1, checks=False), squareform(rdm_2, checks=False))[0])
+        elif eval_method == 'pearson':
+            rdm_corr_null.append(pearsonr(squareform(shuffled_rdm_1, checks=False), squareform(rdm_2, checks=False))[0])
+        elif eval_method == 'euclidean':
+            rdm_cor = np.linalg.norm(squareform(shuffled_rdm_1, checks=False) - squareform(rdm_2, checks=False))
     
     # Calc p value
     rdm_corr_null = np.array(rdm_corr_null)
@@ -44,24 +50,115 @@ def corr_nullDist(rdm_cor, rdm_1, rdm_2, its=100):
     return p_val
 
 
-def calc_rsquared(t, data_shape, feature_rdm, shm, its=10, slide=0):
+def corr_nullDist_rw(vp_scores, design_matrices, eeg_rdm, its=100):
+    """
+        Calculates null distribution based on permutations. 
+    """
+    print("Constructing null distribution")
+    
+    vp_perm_scores = {
+            'u_a': [],
+            'u_s': [],
+            'u_o': [],
+            'os_shared' : [],
+            'sa_shared' : [],
+            'oa_shared' : [],
+            'osa_shared'  : [],
+            'o_total' : [],
+            's_total': [], 
+            'a_total' : [],
+            'all_parts': []
+            }
+    
+    for i in range(its):
+
+        if i % 1000 == 0:
+            print(f'null distr iteration: {i}')
+
+        # Create a random index that respects the structure of an rdm.
+        random.seed(i)
+        shuffle = np.random.choice(eeg_rdm.shape[0], eeg_rdm.shape[0],replace=False)
+
+        shuffled_eeg = eeg_rdm[shuffle, :] #rows
+        shuffled_eeg = shuffled_eeg[:, shuffle] #columns
+        sq_eeg_rdm = squareform(shuffled_eeg, checks=False)
+        
+        # Calc r2 for different models
+        r2_scores = {}
+        models = design_matrices.keys()
+        for model in models:
+            design_matrix = design_matrices[model]
+            model_fit = LinearRegression().fit(design_matrix, sq_eeg_rdm)
+            r2 = model_fit.score(design_matrix, sq_eeg_rdm)
+            r2_scores[model] = r2*100
+        
+        # Perform variance partitioning 
+        u_a_r2 = r2_scores['o-s-a'] - r2_scores['o-s']
+        u_s_r2 = r2_scores['o-s-a'] - r2_scores['o-a']
+        u_o_r2 = r2_scores['o-s-a'] - r2_scores['s-a']
+
+        os_shared_r2 = r2_scores['o-s-a'] - r2_scores['a'] - u_o_r2 - u_s_r2
+        sa_shared_r2 = r2_scores['o-s-a'] - r2_scores['o'] - u_s_r2 - u_a_r2
+        oa_shared_r2 = r2_scores['o-s-a'] - r2_scores['s'] - u_o_r2 - u_a_r2
+
+        osa_shared_r2 = r2_scores['o-s-a'] - u_o_r2 - u_s_r2 - u_a_r2 - os_shared_r2 - sa_shared_r2 - oa_shared_r2
+
+        o_total_r2 = u_o_r2 +  os_shared_r2 + oa_shared_r2 + osa_shared_r2
+        s_total_r2 = u_s_r2 + os_shared_r2 + sa_shared_r2 + osa_shared_r2
+        a_total_r2 = u_a_r2 + oa_shared_r2 + sa_shared_r2 + osa_shared_r2
+
+        all_parts = u_o_r2 + u_s_r2 + u_a_r2 + os_shared_r2 + sa_shared_r2 + oa_shared_r2 + osa_shared_r2
+
+        vp_perm_scores['u_a'].append(u_a_r2)
+        vp_perm_scores['u_s'].append(u_s_r2)
+        vp_perm_scores['u_o'].append(u_o_r2)
+        vp_perm_scores['os_shared'].append(os_shared_r2)
+        vp_perm_scores['sa_shared'].append(sa_shared_r2)
+        vp_perm_scores['oa_shared'].append(oa_shared_r2)
+        vp_perm_scores['osa_shared'].append(osa_shared_r2)
+        vp_perm_scores['o_total'].append(o_total_r2)
+        vp_perm_scores['s_total'].append(s_total_r2)
+        vp_perm_scores['a_total'].append(a_total_r2)
+        vp_perm_scores['all_parts'].append(all_parts)
+
+
+    # Calc p value 
+    p_vals= {}
+    p_vals['u_a'] = np.mean(np.array(vp_perm_scores['u_a']) > vp_scores['u_a'])
+    p_vals['u_s'] = np.mean(np.array(vp_perm_scores['u_s']) > vp_scores['u_s'])
+    p_vals['u_o'] = np.mean(np.array(vp_perm_scores['u_o']) > vp_scores['u_o'])
+    p_vals['os_shared'] = np.mean(np.array(vp_perm_scores['os_shared']) > vp_scores['os_shared'])
+    p_vals['sa_shared'] = np.mean(np.array(vp_perm_scores['sa_shared']) > vp_scores['sa_shared'])
+    p_vals['oa_shared'] = np.mean(np.array(vp_perm_scores['oa_shared']) > vp_scores['oa_shared'])
+    p_vals['osa_shared'] = np.mean(np.array(vp_perm_scores['osa_shared']) > vp_scores['osa_shared'])
+    p_vals['o_total'] = np.mean(np.array(vp_perm_scores['o_total']) > vp_scores['o_total'])
+    p_vals['s_total'] = np.mean(np.array(vp_perm_scores['s_total']) > vp_scores['s_total'])
+    p_vals['a_total'] = np.mean(np.array(vp_perm_scores['a_total']) > vp_scores['a_total'])
+    p_vals['all_parts'] = np.mean(np.array(vp_perm_scores['all_parts']) > vp_scores['all_parts'])
+    
+    return p_vals
+
+
+def calc_rsquared(t, data_shape, dtype, feature_rdm, shm, its=10, eval_method='spearman'):
 
     print(f'Calculating r^2 t= {t}')
 
     existing_shm = shared_memory.SharedMemory(name=shm)
-    eeg_rdms = np.ndarray(data_shape, dtype='float64', buffer=existing_shm.buf)
+    eeg_rdms = np.ndarray(data_shape, dtype=dtype, buffer=existing_shm.buf)
 
     tic = time.time()
 
     # Calc correlation
     eeg_rdm = eeg_rdms[:, :, t]
-    rdm_cor = spearmanr(squareform(eeg_rdm, checks=False), squareform(feature_rdm, checks=False))[0] 
 
-    # Calc significance
-    if slide == 0:
-        rdm_cor_p = corr_nullDist(rdm_cor, eeg_rdm, feature_rdm, its=its) 
-    else:
-        rdm_cor_p = np.nan
+    if eval_method == 'spearman':
+        rdm_cor = spearmanr(squareform(eeg_rdm, checks=False), squareform(feature_rdm, checks=False))[0] 
+    elif eval_method == 'pearson':
+        rdm_cor = pearsonr(squareform(eeg_rdm, checks=False), squareform(feature_rdm, checks=False))[0] 
+    elif eval_method == 'euclidean': 
+        rdm_cor = np.linalg.norm(squareform(eeg_rdm, checks=False) - squareform(feature_rdm, checks=False))
+    
+    rdm_cor_p = corr_nullDist(rdm_cor, eeg_rdm, feature_rdm, its=its, eval_method=eval_method) 
 
     toc = time.time()
 
@@ -70,13 +167,16 @@ def calc_rsquared(t, data_shape, feature_rdm, shm, its=10, slide=0):
     return (rdm_cor, rdm_cor_p)
 
 
-def cor_variability(t, data_shape, feature_rdm, shm, its=100):
+def cor_variability(t, data_shape, dtype, feature_rdm, shm, its=100, eval_method='spearman'):
 
     print(f'Calculating CI t= {t}')
 
     existing_shm = shared_memory.SharedMemory(name=shm)
-    eeg_rdms = np.ndarray(data_shape, dtype='float64', buffer=existing_shm.buf)
+    eeg_rdms = np.ndarray(data_shape, dtype=dtype, buffer=existing_shm.buf)
     eeg_rdm = eeg_rdms[:, :, t]
+
+    eeg_rdm_vec = squareform(eeg_rdm, checks=False)
+    feature_rdm_vec = squareform(feature_rdm, checks=False)
 
     tic = time.time()
 
@@ -88,18 +188,36 @@ def cor_variability(t, data_shape, feature_rdm, shm, its=100):
             print(f'cor var iteration: {i}')
 
         # Create a random index that respects the structure of an rdm.
-        sample = np.random.choice(eeg_rdm.shape[0], eeg_rdm.shape[0],replace=True) 
+        sample = np.random.choice(eeg_rdm_vec.shape[0], eeg_rdm_vec.shape[0],replace=True) 
 
         # Subsample from both the reference and the feature RDM
-        eeg_rdm_sample = eeg_rdm[sample] 
-        feature_rdm_sample = feature_rdm[sample] 
+        eeg_rdm_sample = eeg_rdm_vec[sample] 
+        feature_rdm_sample = feature_rdm_vec[sample] 
 
         # correlating with neural similarty matrix
-        rdm_corr_boots.append(spearmanr(squareform(eeg_rdm_sample, checks=False), squareform(feature_rdm_sample, checks=False))[0])
+        if eval_method == 'spearman':
+            rdm_corr_boots.append(spearmanr(eeg_rdm_sample, feature_rdm_sample)[0])
+        elif eval_method == 'pearson':
+            rdm_corr_boots.append(pearsonr(eeg_rdm_sample, feature_rdm_sample)[0])
+        elif eval_method == 'euclidean':
+            rdm_corr_boots.append(np.linalg.norm(eeg_rdm_sample - feature_rdm_sample))
+
 
     # Get 95% confidence interval
     lower_p = np.percentile(rdm_corr_boots, 2.5)
     upper_p = np.percentile(rdm_corr_boots, 97.5)
+
+    # # Temp
+    # fig, axes = plt.subplots(dpi=300)
+    # sns.displot(rdm_corr_boots, bins=50)
+    # axes.set_title(f'Bootstrap dist sub {sub}, t={t}')
+    # axes.set_ylabel('Spearman')
+
+    # fig.tight_layout()
+
+    # img_path = f'/scratch/azonneveld/rsa/fusion/eeg-model/standard/plots/z_0/sub-01/cor-dist-{t}'
+    # plt.savefig(img_path)
+    # plt.clf()
 
     toc = time.time()
     print(f'iteration {t} in {toc-tic}')
@@ -107,12 +225,13 @@ def cor_variability(t, data_shape, feature_rdm, shm, its=100):
     return (lower_p, upper_p)
 
 
-def calc_rsquared_rw(t, data_shape, design_matrix, shm):
+
+def calc_rsquared_rw(t, data_shape, dtype, design_matrix, shm, ridge, cv):
 
     print(f'Calculating r^2 t= {t}')
 
     existing_shm = shared_memory.SharedMemory(name=shm)
-    eeg_rdms = np.ndarray(data_shape, dtype='float64', buffer=existing_shm.buf)
+    eeg_rdms = np.ndarray(data_shape, dtype=dtype, buffer=existing_shm.buf)
 
     tic = time.time()
 
@@ -120,38 +239,95 @@ def calc_rsquared_rw(t, data_shape, design_matrix, shm):
     sq_eeg_rdm  = squareform(eeg_rdm, checks=False)
 
     # Reweighting of model rdms using multiple linear regression
-    model = LinearRegression().fit(design_matrix, sq_eeg_rdm)
-
-    # Predict 
-    r2_score = model.score(design_matrix, sq_eeg_rdm)
+    if ridge == 0 and cv == 0:
+        model = LinearRegression().fit(design_matrix, sq_eeg_rdm)
+        r2 = model.score(design_matrix, sq_eeg_rdm)
+    if ridge == 0 and cv == 1:
+        kf = KFold(n_splits=sq_eeg_rdm.shape[0])
+        r2_scores = []
+        for train, test in kf.split(sq_eeg_rdm):
+            X_train, X_test, Y_train, Y_test = design_matrix[train], design_matrix[test], sq_eeg_rdm[train], sq_eeg_rdm[test]
+            model = LinearRegression().fit(X_train, Y_train)
+            predictions = model.predict(X_test)
+            r2 = r2_score(Y_test, predictions)
+            r2_scores.append(r2)
+        r2 = np.mean(r2_scores)
+    if ridge == 1 and cv == 0:
+        model = Ridge().fit(design_matrix, sq_eeg_rdm)
+        r2 = model.score(design_matrix, sq_eeg_rdm)
+    if ridge == 1 and cv == 1:
+        kf = KFold(n_splits=sq_eeg_rdm.shape[0])
+        r2_scores = []
+        for train, test in kf.split(sq_eeg_rdm):
+            X_train, X_test, Y_train, Y_test = design_matrix[train], design_matrix[test], sq_eeg_rdm[train], sq_eeg_rdm[test]
+            model = LinearRegression().fit(X_train, Y_train)
+            predictions = model.predict(X_test)
+            r2 = r2_score(Y_test, predictions)
+            r2_scores.append(r2)
+        r2 = np.mean(r2_scores)
+    
     rdm_cor_p = np.nan
 
-    return (r2_score, rdm_cor_p)
+    toc = time.time()
+    print(f't= {t} done in {toc-tic}')
+
+    return (r2, rdm_cor_p)
 
 
-def shared_var_plot(shared_cor_dict, times, sub, res_folder, method):
-    combs = ['o-s', 'o-a', 's-a','o-s-a']
+def calc_rsquared_rw_2(t, data_shape, dtype, design_matrices, its, shm):
 
-    colours = ['b', 'r', 'g', 'orange']
-    fig, ax = plt.subplots(dpi=300)
-    for i in range(len(combs)):
-        comb = combs [i]
-        colour = colours[i]
+    print(f'Calculating r^2 t= {t}')
+    tic = time.time()
 
-        stats_df = pd.DataFrame()
-        stats_df['cors'] = shared_cor_dict[comb]
-        stats_df['times'] = times
-        ax.plot(stats_df['times'], stats_df['cors'], label=comb, color=colour)
+    existing_shm = shared_memory.SharedMemory(name=shm)
+    eeg_rdms = np.ndarray(data_shape, dtype=dtype, buffer=existing_shm.buf)
+    eeg_rdm = eeg_rdms[:, :, t]
+    sq_eeg_rdm  = squareform(eeg_rdm, checks=False)
 
-    ax.axvline(x=0, color='gray', alpha=0.5, linestyle='--')
-    ax.axvline(x=3, color='gray', alpha=0.5, linestyle='--')
-    ax.set_title(f'EEG-model relation sub {sub}')
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Shared variance explained (%)')
-    ax.legend()
-    fig.tight_layout()
+    # Calculate scores for all models
+    r2_scores = {}
+    models = design_matrices.keys()
+    for model in models:
+        design_matrix = design_matrices[model]
 
-    img_path = res_folder + f'{sub}_{method}_shared_r2.png'
-    plt.savefig(img_path)
-    plt.clf()
+        model_fit = LinearRegression().fit(design_matrix, sq_eeg_rdm)
+        r2 = model_fit.score(design_matrix, sq_eeg_rdm)
+        r2_scores[model] = r2*100
     
+    # Perform variance partitioning 
+    u_a_r2 = r2_scores['o-s-a'] - r2_scores['o-s']
+    u_s_r2 = r2_scores['o-s-a'] - r2_scores['o-a']
+    u_o_r2 = r2_scores['o-s-a'] - r2_scores['s-a']
+
+    os_shared_r2 = r2_scores['o-s-a'] - r2_scores['a'] - u_o_r2 - u_s_r2
+    sa_shared_r2 = r2_scores['o-s-a'] - r2_scores['o'] - u_s_r2 - u_a_r2
+    oa_shared_r2 = r2_scores['o-s-a'] - r2_scores['s'] - u_o_r2 - u_a_r2
+    osa_shared_r2 = r2_scores['o-s-a'] - u_o_r2 - u_s_r2 - u_a_r2 - os_shared_r2 - sa_shared_r2 - oa_shared_r2
+
+    o_total_r2 = u_o_r2 +  os_shared_r2 + oa_shared_r2 + osa_shared_r2
+    s_total_r2 = u_s_r2 + os_shared_r2 + sa_shared_r2 + osa_shared_r2
+    a_total_r2 = u_a_r2 + oa_shared_r2 + sa_shared_r2 + osa_shared_r2
+    all_parts = u_o_r2 + u_s_r2 + u_a_r2 + os_shared_r2 + sa_shared_r2 + oa_shared_r2 + osa_shared_r2
+
+    vp_scores = {
+        'u_a': u_a_r2,
+        'u_s': u_s_r2,
+        'u_o': u_o_r2,
+        'os_shared' : os_shared_r2,
+        'sa_shared' : sa_shared_r2,
+        'oa_shared' : oa_shared_r2,
+        'osa_shared' : osa_shared_r2,
+        'o_total': o_total_r2,
+        's_total': s_total_r2,
+        'a_total' : a_total_r2,
+        'all_parts' : all_parts
+    }
+
+    p_vals = corr_nullDist_rw(vp_scores, design_matrices, eeg_rdm, its=its)
+
+    return (vp_scores, p_vals)
+
+
+
+
+
